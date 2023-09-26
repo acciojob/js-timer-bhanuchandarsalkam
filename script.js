6cb5afceb288e885b7fcd2afd1ce19708118dff6
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-09-26 20:42:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// write js code here if required
+const now=new Date();
+const hours=now.getHours();
+const minutes=now.getMinutes();
+const seconds=now.getSeconds();
+document.getElementById("timer").innerText=`${hours}:${minutes}:${seconds}`
